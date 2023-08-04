537ada6cf43472fcd55c8259426f3596653c00fb
skystar200 <skystar20095@gmail.com>
2023-08-04 19:38:58 +0900

    Feat : 8/3 배열 실습
    변수, 상수, 배열
    03-Javascript/연습-01,02

---
+++ class/signup/03-javascript/연습-01-array.js
@@ -0,0 +1,18 @@
+let classmates = ["철수", "영희", "훈이"]
+// undefined
+classmates
+// (3) ['철수', '영희', '훈이']
+classmates[0]
+// '철수'
+classmates.includes("훈이")
+// true
+classmates.length
+// 3
+classmates.push("맹구")
+// 4
+classmates
+// (4) ['철수', '영희', '훈이', '맹구']
+classmates.pop()
+// '맹구'
+classmates
+// (3) ['철수', '영희', '훈이']
