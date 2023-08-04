7639a626829ce622268852fc384708eac2582f24
skystar200 <skystar20095@gmail.com>
2023-08-05 02:02:37 +0900

    Feat : 8/4 객체, 데이터 타입 실습

---
+++ class/signup/03-javascript/연습-03-objact.js
@@ -0,0 +1,11 @@
+let friend={name : "철수", 
+            age : 13, 
+            camp : "코드캠프"
+           }
+// undefined
+friend
+// {name: '철수', age: 13, camp: '코드캠프'}
+friend.na
+// undefined
+friend.name
+// '철수'
---
+++ class/signup/03-javascript/연습-04-object-in-arr.js
@@ -0,0 +1,14 @@
+let classmates = [
+    { name : "철수", age : 13, school : "다람쥐 초등학교"},
+    { name : "영희", age : 33, school : "공룡 초등학교"},
+    { name : "영지", age : 22, school : "메롱 초등학교"}
+]
+// undefined
+classmates
+// (3) [{…}, {…}, {…}]0: {name: '철수', age: 13, school: '다람쥐 초등학교'}1: {name: '영희', age: 33, school: '공룡 초등학교'}2: {name: '영지', age: 22, school: '메롱 초등학교'}length: 3[[Prototype]]: Array(0)
+classmates.length
+// 3
+classmates[0].school
+// '다람쥐 초등학교'
+classmates[2].age
+// 22
